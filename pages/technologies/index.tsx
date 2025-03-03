cc49051d35cbb52ba0c13c57494f07b6afdd4fc4
mahmoud2391997 <mahmoudmelsaid1@gmail.com>
2025-03-04 01:00:27 +0200

    adding project

--- pages/technologies/index.tsx
+++ pages/technologies/index.tsx
@@ -2,7 +2,7 @@ export default function TechnologiesPage() {
   return (
     <div className="grid lg:grid-cols-5 lg:w-[80%] xl:w-[70%] w-[90%] md:grid-cols-3 sm:grid-cols-2 grid-cols-1 h-auto my-36 gap-6 text-white lg:my-auto">
       <section className=" bg-[#7c7c7c] rounded w-full flex flex-col justify-between h-[300px]">
-        <h2 className="text-2xl w-full text-center">
+        <h2 className="text-xl w-full text-center">
           Client Side Technologies
         </h2>
         <ul className=" list-disc list-inside flex flex-col w-[80%] h-[60%] justify-between m-auto ">
@@ -13,7 +13,7 @@ export default function TechnologiesPage() {
         </ul>
       </section>
       <section className=" bg-[#7c7c7c] rounded w-full flex flex-col justify-between h-[300px]">
-        <h2 className="text-2xl w-full text-center">CSS Frameworks</h2>
+        <h2 className="text-xl w-full text-center">CSS Frameworks</h2>
         <ul className=" list-disc list-inside flex flex-col w-[80%] h-[60%] justify-between m-auto ">
           <li>Tailwind CSS</li>
           <li>Bootstrap</li>
@@ -21,7 +21,7 @@ export default function TechnologiesPage() {
         </ul>
       </section>
       <section className=" bg-[#7c7c7c] rounded w-full flex flex-col justify-between h-[300px]">
-        <h2 className="text-2xl w-full text-center">Front-End Frameworks</h2>
+        <h2 className="text-xl w-full text-center">Front-End Frameworks</h2>
         <ul className=" list-disc list-inside flex flex-col w-[80%] h-[60%] justify-between m-auto ">
           <li>React.Js</li>
           <li>React.Ts</li>
@@ -30,7 +30,7 @@ export default function TechnologiesPage() {
         </ul>
       </section>
       <section className=" bg-[#7c7c7c] rounded w-full flex flex-col justify-between h-[300px]">
-        <h2 className="text-2xl w-full text-center">
+        <h2 className="text-xl w-full text-center">
           Backend Languages and Frameworks
         </h2>
         <ul className=" list-disc list-inside flex flex-col w-[80%] h-[60%] justify-between m-auto ">
@@ -41,7 +41,7 @@ export default function TechnologiesPage() {
         </ul>
       </section>
       <section className=" bg-[#7c7c7c] rounded w-full flex flex-col justify-between h-[300px]">
-        <h2 className="text-2xl w-full text-center">Databases</h2>
+        <h2 className="text-xl w-full text-center">Databases</h2>
         <ul className=" list-disc list-inside flex flex-col w-[80%] h-[60%] justify-between m-auto ">
           <li>MongoDB</li>
           <li>My SQL</li>
